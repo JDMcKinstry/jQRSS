516831ca13cc5228b75581c32029f76b86ee18dc
JD McKinstry <jdmckinstry@gmail.com>
2016-04-25 16:09:07 -0400

    Update jQRSS.js

--- jQRSS.js
+++ jQRSS.js
@@ -1,4 +1,4 @@
-;;(function($, $fn) {	//	see also: https://developers.google.com/feed/v1/jsondevguide#using_json
+(function($, $fn) {	//	see also: https://developers.google.com/feed/v1/jsondevguide#using_json
 	if ($.jQRSS) return $.jQRSS;
 		
 	/**	b64EncodeUnicode(str)
@@ -121,7 +121,7 @@
 					if (props.doLog) $.jQRSS.debug("REQUEST RSS SUCCESS", { data: data, textStatus: textStatus, jqXHR: jqXHR, feed: feed, entries: entries });
 					
 					if (!cbList) return { data: data, textStatus: textStatus, jqXHR: jqXHR, feed: f, entries: e };
-					$.jQRSS.debug("cbList", cbList);
+					
 					return $.each(cbList, function(k, v) {
 						if (typeof v == 'function') return v.call(this, feed ? feed : data, entries ? entries : null);
 					});
@@ -178,3 +178,192 @@
 	$.extend({ jQRSS: jQRSS });
 	
 })(jQuery, jQuery.fn);
+
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - BEGIN PLUGIN- - - - - - - - - */
+/* - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - NOW HOSTED AT - - - - - - - - */
+/* - - -https://github.com/JDMcKinstry/jQRSS - - - */
+/* - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - -END PLUGIN - - - - - - - - - */
+/* - - - - - - - - - - - - - - - - - - - - - - - - */
+
+function getFeedNewsBlock() {
+	var url = 'http://www.army.mil/rss/233/',
+		props = { count: 8 };
+	$.jQRSS(url, props, function (newsFeed) {
+		if (!newsFeed) return false;
+		$.jQRSS.debug('[getFeedNewsBlock()]', newsFeed);
+		var nbc = $(".news-block .content").empty().append($("<div/>").addClass("expanded"), $("<div />").css("clear", "both"), $("<ul/>"));
+		for (var i = 0; i < newsFeed.entries.length; i++) {
+			var entry = newsFeed.entries[i];
+
+			var author = entry['author'] ? entry['author'] : "",
+				categories = entry['categories'] ? entry['categories'] : new Array(),
+				content = entry['content'] ? entry['content'] : "",
+				contentSnippet = entry['contentSnippet'] ? entry['contentSnippet'] : "",
+				link = entry['link'] ? entry['link'] : "",
+				publishedDate = entry['publishedDate'] ? entry['publishedDate'] : "",
+				title = entry['title'] ? entry['title'] : "";
+
+			/*  author, categories, content, contentSnippet, link, publishedDate, title  */
+			if (i == 0) {
+				var patt = new RegExp(' [0-9][0-9]:[0-9][0-9]:[0-9][0-9] [-|+][0-9][0-9][0-9][0-9]');
+				nbc.children(".expanded").prepend($("<span />").text(publishedDate.replace(patt, "")));
+
+				nbc.children(".expanded").append(
+					$("<h4 />").append($("<a />", { href: link, target: "_blank" }).text(title)),
+					$("<p />").text(content)
+				);
+
+				nbc.children(".expanded").find("p").last().append($("<a />", { href: link, target: "_blank" }).text(" Story »"));
+			}
+			else {
+				nbc.children("ul").append($("<li />").append($("<a />", { href: link, target: "_blank" }).text(title)));
+			}
+		}
+	});
+}
+
+getFeedNewsBlock();
+$("#btnUpdateNewsBlock").on("click", function (e) { 
+    var bakUp = $(".news-block .content").html();
+    $(".news-block .content").empty();
+    setTimeout(function () { getFeedNewsBlock(); }, 1000);
+    if ($(".news-block .content").children.length == 0) $(".news-block .content").html(bakUp);
+});
+
+function getObjectLength(obj) {
+    if (typeof obj != "object") return false;
+    var i = 0;
+    for (x in obj) i++;
+    return i;
+}
+
+function createFeatures(targetParent) {
+    var features = {},
+		url = 'http://api.flickr.com/services/feeds/photos_public.gne?id=58792031@N04&lang=en-us&format=rss_200',
+		props = { count: 6, callbacks: [ function(d) { $.jQRSS.debug('this is just a test of using an array of callbacks', d) } ] };
+    $.jQRSS(url, props, function (newsFeed) {
+        if (!newsFeed) return false;
+        $.jQRSS.debug('[createFeatures()]', newsFeed);
+        var entries;
+        if (newsFeed["entries"]) { entries = newsFeed.entries } else { return false; }
+        for (x in entries) {
+            var entry = entries[x];
+            features[x] = {};
+            features[x].title = entry.title;
+            features[x].image = {
+                alt: entry.contentSnippet,
+                nfo: entry.mediaGroups[0].contents[0].credits[0].content + " "
+            + entry.content.substr(
+                entry.content.indexOf("(Photo by "),
+                entry.content.substring(entry.content.indexOf("(Photo by ")).indexOf("</p>")
+            ).replace("(", "").replace(")", ""),
+                src: entry.mediaGroups[0].contents[0].url,
+                title: entry.title
+            };
+            features[x].blurb = entry.mediaGroups[0].contents[0].description;
+            features[x].links = {};
+            features[x].links[0] = {
+                src: entry.link,
+                target: "_blank",
+                text: "flickr"
+            };
+        }
+        
+        var ulIndex = $("<ul />").addClass("index");
+        if (getObjectLength(features) > 0) {
+            for (x in features) {
+                var block = features[x],
+                    liIndex = $("<li />").append($("<a />").prop("href", "javascript:void(0);")).appendTo(ulIndex);
+                feature = $("<div />").addClass("feature").appendTo(targetParent);
+    
+                if (x == 0) {
+                    feature.addClass("selected");
+                    liIndex.addClass("selected");
+                }
+    
+                if (block["title"]) {
+                    $("<h2 />").addClass("title").text(block["title"]).prependTo(feature);
+                }
+    
+                if (block["image"]) {
+                    var image = $("<div />").addClass("image").appendTo(feature);
+                    image.prepend($("<img />"));
+                    image.append($("<p />"));
+                    if (block["image"]["alt"]) {
+                        image.children("img").prop("alt", block["image"]["alt"]);
+                    }
+                    if (block["image"]["nfo"]) {
+                        image.children("p").text(block["image"]["nfo"]);
+                    }
+                    if (block["image"]["src"]) {
+                        image.children("img").prop("src", block["image"]["src"]);
+                    }
+                    if (block["image"]["title"]) {
+                        image.children("img").prop("title", block["image"]["title"]);
+                    }
+                }
+    
+                if (block["blurb"]) {
+                    $("<p />").addClass("blurb").text(block["blurb"]).appendTo(feature);
+                }
+    
+                if (block["links"]) {
+                    if (block["links"][0]) {
+                        var links = $("<ul />").addClass("links").appendTo(feature);
+                        for (y in block["links"]) {
+                            var link = block["links"][y],
+                                li = $("<li />").append($("<a />")).appendTo(links);
+                            if (link["src"]) {
+                                li.children("a").prop("href", link["src"]);
+                            }
+                            if (link["target"]) {
+                                li.children("a").prop("target", link["target"]);
+                            }
+                            if (link["text"]) {
+                                li.children("a").text(link["text"]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        ulIndex.appendTo(targetParent);
+    });
+}
+
+var tmrFeatures, subTmrFeatures;
+function goToNextFeature() {
+    var curFeature = $("#TopFeaturesBlock .feature.selected").removeClass("selected"),
+        curIndex = $("#TopFeaturesBlock .index .selected").removeClass("selected"),
+        nexFeature = (curFeature.nextAll('div').first().length) ? curFeature.nextAll('div').first() : $("#TopFeaturesBlock .feature").first(),
+        nexIndex = $("#TopFeaturesBlock .index li").filter(function (i) { return i == nexFeature.index(); });
+
+    nexFeature.addClass("selected");
+    nexIndex.addClass("selected");
+
+    curFeature.stop().fadeOut("slow");
+    nexFeature.stop().fadeIn("slow");
+};
+function featureIndex_Click(e) {
+    var $this = $(this),
+        oldFeature = $("#TopFeaturesBlock .feature.selected").removeClass("selected"),
+        newFeature = $("#TopFeaturesBlock .feature").filter(function (i) { return i == $this.parent().index(); }).addClass("selected");
+
+    $("#TopFeaturesBlock .index li").removeClass("selected")
+        .filter(function (i) { return i == $this.parent().index(); }).addClass("selected");
+    oldFeature.stop().fadeOut("slow");
+    newFeature.stop().fadeIn("slow");
+
+    clearInterval(tmrFeatures);
+    clearTimeout(subTmrFeatures);
+    subTmrFeatures = setTimeout(function () { tmrFeatures = setInterval(function () { goToNextFeature(); }, 5000); }, 15000);
+};
+
+createFeatures($("#TopFeaturesBlock").empty());
+$("#TopFeaturesBlock .index a").on("click", featureIndex_Click);
+tmrFeatures = setInterval(function () { goToNextFeature(); }, 5000);
